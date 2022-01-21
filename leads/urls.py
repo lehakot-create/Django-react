8e295d7774ef4f56b97fd181e203e23c99c5df50
lehakot-create <lehakot85@inbox.ru>
2022-01-21 16:36:29 +0400

    add update, delete views

--- leads/urls.py
+++ leads/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/lead/', views.LeadListCreate.as_view()),
+    path('api/lead/<int:pk>', views.LeadUpdateDelete.as_view()),
 ]
--- leads/views.py
+++ leads/views.py
@@ -6,3 +6,8 @@ from rest_framework import generics
 class LeadListCreate(generics.ListCreateAPIView):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
+
+
+class LeadUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
